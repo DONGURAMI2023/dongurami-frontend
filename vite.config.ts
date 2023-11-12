24528000390c3a840883617a73f86301686594ec
Henry-Hong <hhr8540@naver.com>
2023-11-12 22:31:17 +0900

    Feat : PWA 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,34 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["logo.png"],
+      manifest: {
+        display: "fullscreen",
+        name: "시티마블",
+        short_name: "시티마블",
+        description: "City-Marble",
+        start_url: "/",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "logo.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
 });
